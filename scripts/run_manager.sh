c67eb9a087de4c0d015e136336636611f7dcb08a
CZXgodlike <997232782@qq.com>
2021-12-07 19:07:18 -0500

    added automated scripts

---
+++ scripts/run_manager.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+wget https://github.com/momoxiaocaiji/CS655-final/archive/refs/heads/jar.zip
+unzip -q -o jar.zip
+mv -f CS655-final-jar/service-0.0.3.jar .
+mv -f CS655-final-jar/application.yml .
+rm -r CS655-final-jar
+rm jar.zip
+java -jar service-0.0.3.jar
---
+++ scripts/run_worker.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+wget https://github.com/momoxiaocaiji/CS655-final/archive/refs/heads/worker.zip
+unzip -q -o worker.zip
+mv -f CS655-final-worker/Worker .
+rm -r CS655-final-worker
+rm worker.zip
+cd Worker/src
+javac -cp .:../lib/commons-codec-1.9.jar worker.java
+java -cp .:..lib/commons-codec-1.9.jar worker
